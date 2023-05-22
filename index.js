ef5033fb19428236f219b3121b26cccbc19fb0a2
Dzalev <stefan.dzalev@itgma.com>
2023-05-22 13:50:59 +0200

    tess

--- index.js
+++ index.js
@@ -1,36 +1,58 @@
-const ViberBot = require('viber-bot').Bot,
-  BotEvents = require('viber-bot').Events,
-  TextMessage = require('viber-bot').Message.Text,
-  express = require('express');
+const ViberBot  = require('viber-bot').Bot;
+const BotEvents = require('viber-bot').Events;
+const TextMessage = require('viber-bot').Message.Text;
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
 const app = express();
 
-// if (!process.env.BOT_ACCOUNT_TOKEN) {
-//   console.log('Could not find bot account token key.');
-//   return;
-// }
-// if (!process.env.EXPOSE_URL) {
-//   console.log('Could not find exposing url');
-//   return;
-// }
-
 const bot = new ViberBot({
-  authToken:'50ae53fb49a7e4df-be3a44ea4035ad6-b74326b08da78cad',
-  name: "Seljak",
-  avatar: "https://upload.wikimedia.org/wikipedia/commons/3/3d/Katze_weiss.png"
+    authToken:'50ae53fb49a7e4df-be3a44ea4035ad6-b74326b08da78cad',
+    name: "Seljak",
+    avatar: "https://upload.wikimedia.org/wikipedia/commons/3/3d/Katze_weiss.png"
 });
+
 bot.on(BotEvents.SUBSCRIBED, response => {
-  response.send(new TextMessage(`Hi there ${response.userProfile.name}. I am ${bot.name}! Feel free to ask me anything.`));
+    response.send(new TextMessage(`Hi there ${response.userProfile.name}. I am ${bot.name}! Feel free to ask me anything.`));
 });
-bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
-  response.send(new TextMessage(`Куро да ми јајш`));
+
+bot.on(BotEvents.MESSAGE_RECEIVED, async (message, response) => {
+    try {
+        // Fetch the website HTML
+        const websiteHtml = await axios.get('https://www.mylpg.eu/stations/macedonia/prices/');
+
+        // Parse HTML and extract the gas prices
+        const $ = cheerio.load(websiteHtml.data);
+        let gasPrices = '';
+
+        $('tr').each((index, element) => {
+            const price = $(element).find('td:nth-child(3)').text();
+            const date = $(element).find('td:nth-child(1)').text();
+            if(price && date) {
+                gasPrices += `Date: ${date}, Price: ${price}\n`;
+            }
+        });
+
+        // Create a message
+        const msg = new TextMessage(`Current gas prices are: \n${gasPrices}`);
+
+        // Send the message to the user
+        response.send(msg);
+    } catch (error) {
+        console.error(error);
+        response.send(new TextMessage(`Sorry, I couldn't fetch the gas prices at the moment.`));
+    }
 });
+
 const port = process.env.PORT || 3000;
+
 app.use("/viber/webhook", bot.middleware());
+
 app.listen(port, () => {
-  console.log(`Application running on port: ${port}`);
-  bot.setWebhook(`https://viber-bot-2l7c.onrender.com/viber/webhook`).catch(error => {
-    console.log('Can not set webhook on following server. Is it running?');
-    console.error(error);
-    process.exit(1);
-  });
+    console.log(`Application running on port: ${port}`);
+    bot.setWebhook(`https://viber-bot-2l7c.onrender.com/viber/webhook`).catch(error => {
+        console.log('Can not set webhook on following server. Is it running?');
+        console.error(error);
+        process.exit(1);
+    });
 });
